2d535ff81f5484af4df4df3debd26c05918ee0ea
Jason Pitts <juniorjr083@gmail.com>
2018-09-20 01:09:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,23 @@ fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+      if(typeof collection == array){
+        for(let i = 0; i < collection.length;i++){
+         
+          
+        }
+         return callback(collection[i]);
+        
+      }
+      else{
+        for(let key in collection){
+          
+        }
+        return callback(collection[key]); 
+      }
+      
+      
     },
 
     map: function() {
